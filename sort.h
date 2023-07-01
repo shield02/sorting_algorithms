bd9b1f87c1214a2feb132bbccc347bb953796e66
shield <myshield02@gmail.com>
2023-07-01 05:39:31 +0100

    included deck header file and added prototype for sort_deck

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "deck.h"
 
 /**
  * struct listint_s - Doubly linked list node
@@ -32,5 +33,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 #endif
